aac1064ab85f9fc1867c055fc5caa3ee92211538
daredakke <77235881+daredakke@users.noreply.github.com>
2024-03-02 09:02:46 +0000

    Test screen shake

--- game.gd
+++ game.gd
@@ -8,25 +8,50 @@ enum Mode {
 	FULLSCREEN,
 }
 
+const SHAKE_SPEED: float = 30.0
+const SHAKE_STRENGTH: float = 8.25
+const SHAKE_DECAY_RATE: float = 10
+
+@export var camera: Camera2D
+
 var _current_mode: int = Mode.WINDOW_ONE
 var _resolutions: Array[Vector2i] = [
 	Vector2i(640, 360),
 	Vector2i(1280, 720),
 ]
+var _shake_speed: float:
+	set(value):
+		_shake_speed = clampf(value, 0.0, 100.0)
+var _noise_i: float = 0.0
+var _shake_strength: float = 0.0:
+	set(value):
+		_shake_strength = clampf(value, 0.0, 100.0)
+
+@onready var _rand = RandomNumberGenerator.new()
+@onready var _noise = FastNoiseLite.new()
 
 
 func _ready() -> void:
+	_rand.randomize()
+	_noise.seed = randi()
+	_noise.frequency = 0.5
+	
 	_resize_screen(_current_mode)
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("shake"):
+		_shake_screen(SHAKE_STRENGTH, SHAKE_SPEED)
+	
+	if Input.is_action_just_pressed("resize"):
 		_current_mode += 1
 		
 		if _current_mode >= Mode.size():
 			_current_mode = 0
 		
 		_resize_screen(_current_mode)
+	
+	camera.offset = _screen_shake_decay(delta, SHAKE_DECAY_RATE)
 
 
 func _resize_screen(mode: int) -> void:
@@ -38,3 +63,18 @@ func _resize_screen(mode: int) -> void:
 			get_window().move_to_center()
 		2:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+
+
+func _shake_screen(strength: float, speed: float) -> void:
+	_shake_strength = strength
+	_shake_speed = speed
+
+
+func _screen_shake_decay(delta: float, decay_rate: float) -> Vector2:
+	_shake_strength = lerp(_shake_strength, 0.0, decay_rate * delta)
+	_noise_i += delta * _shake_speed
+	
+	return Vector2(
+		_noise.get_noise_2d(1, _noise_i) * _shake_strength,
+		_noise.get_noise_2d(100, _noise_i) * _shake_strength
+	)
